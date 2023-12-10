6fd257724e4629102ce39c9ddbb142c7245bfaab
Michael Kronenberg <mk@my1cloud.net>
2023-12-10 16:55:24 -0700

    Grid appears on load

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // get elements
 const mainGrid = document.querySelector('#main-grid');
+const fragment = new DocumentFragment();
+
+const calcGrid = () => {
+      const verticalSquares = Math.floor(window.innerHeight / 16);
+      const horizontalSquares = Math.floor(window.innerWidth / 16);
+      const totalSquares = verticalSquares*horizontalSquares;
+      return totalSquares;
+};
+
+const squaresNeeded = calcGrid();
+
+for (let index = 0; index < squaresNeeded; index++) {
+      const gridSquare = document.createElement('div');
+      gridSquare.innerHTML = `<div class="grid-box"></div>`;
+      fragment.appendChild(gridSquare);
+}
+
+mainGrid.appendChild(fragment);
