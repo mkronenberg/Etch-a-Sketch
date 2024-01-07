68b6855085d125b7a5e4ec9fa241b5550f6ad4f6
Michael Kronenberg <mk@my1cloud.net>
2024-01-07 15:42:15 -0700

    resize and draw working

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
 // get elements
+const header = document.querySelector('#header');
 const mainGrid = document.querySelector('#main-grid');
 const fragment = new DocumentFragment();
+const redrawBtn = document.querySelector('#redraw');
 
-const calcGrid = () => {
-      const verticalSquares = Math.floor(window.innerHeight / 16);
-      const horizontalSquares = Math.floor(window.innerWidth / 16);
-      const totalSquares = verticalSquares*horizontalSquares;
-      return totalSquares;
-};
+// slider display
+const pixelValue = document.querySelector("#pixel-value");
+const input = document.querySelector("#tick");
+pixelValue.textContent = input.value;
+input.addEventListener("input", (event) => {
+      pixelValue.textContent = event.target.value;
+});
 
-const squaresNeeded = calcGrid();
 
-for (let index = 0; index < squaresNeeded; index++) {
-      const gridSquare = document.createElement('div');
-      gridSquare.innerHTML = `<div class="grid-box"></div>`;
-      fragment.appendChild(gridSquare);
+const drawGrid = () => {
+
+      const gridPixelSize = parseInt(input.value);
+      mainGrid.style.setProperty("grid-template-columns", `repeat(auto-fill, ${gridPixelSize}px)`);
+      mainGrid.innerHTML = ``;
+
+      const calcGrid = () => {
+            const verticalSquares = Math.floor((window.innerHeight-header.offsetHeight) / gridPixelSize);
+            const horizontalSquares = Math.floor(window.innerWidth / gridPixelSize);
+            const totalSquares = verticalSquares*horizontalSquares;
+            return totalSquares;
+      };
+
+      const squaresNeeded = calcGrid();
+
+      for (let index = 0; index < squaresNeeded; index++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.innerHTML = `<div class="grid-box" style="width: ${gridPixelSize}px; height: ${gridPixelSize}px;"></div>`;
+            fragment.appendChild(gridSquare);
+      }
+
+      mainGrid.appendChild(fragment);
+
+      const gridBox = document.querySelectorAll('.grid-box');
+
+      gridBox.forEach((box) => {
+            box.addEventListener("mouseenter", () => {
+                  box.classList.add("activated");
+            });
+      });
+
 }
 
-mainGrid.appendChild(fragment);
+drawGrid();
+
+window.addEventListener("resize", () => {drawGrid()});
+redraw.addEventListener("click", () => {drawGrid()});
+
